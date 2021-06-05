fc74b160af59893268bfb22c9a25a7065a94bd08
Sanket Shirsat <67065198+sanketshirsat@users.noreply.github.com>
2021-06-05 11:07:49 +0530

    Stating with spring boot project
    Stating with spring boot project

---
+++ src/test/java/com/sanket/mrmanager/StockManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket.mrmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
